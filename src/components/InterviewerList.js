fd6077be3c724254a810ceb45e3f90855e424672
clarchiu <clarchiu@gmail.com>
2020-12-14 03:39:54 -0800

    added interviewerlsit + styles

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const { interviewers, value, onChange } = props;
+
+  const interviewerItems = interviewers.map(interviewerData => {
+    const { id, name, avatar } = interviewerData;
+    return (
+      <InterviewerListItem
+        key={id}
+        name={name}
+        avatar={avatar}
+        selected={id === value}
+        setInterviewer={e => onChange(id)}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerItems}
+      </ul>
+    </section>
+  );
+};
